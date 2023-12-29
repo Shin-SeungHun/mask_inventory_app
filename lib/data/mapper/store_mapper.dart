6aec390fe55e7589c3485646bd18627f51c97e22
shin-SeungHun <sng7387@gmail.com>
2023-12-29 11:27:47 +0900

    model에 데이터 전달 위한 mapper 작성

---
+++ lib/data/mapper/store_mapper.dart
@@ -0,0 +1,18 @@
+import '../dto/mask_dto.dart';
+import '../model/store.dart';
+
+extension DtoToModel on Stores {
+  Store toStore() {
+    return Store(
+      addr: addr ?? '',
+      code: code ?? '',
+      createdAt: createdAt ?? '',
+      lat: lat ?? 0,
+      lng: lng ?? 0,
+      name: name ?? '',
+      remainStat: remainStat ?? '',
+      stockAt: stockAt ?? '',
+      type: type ?? '',
+    );
+  }
+}

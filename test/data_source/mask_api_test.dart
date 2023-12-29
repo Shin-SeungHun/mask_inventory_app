350e3a04e230fef16107871caf3fa993416bce18
shin-SeungHun <sng7387@gmail.com>
2023-12-29 10:30:54 +0900

    dto에 데이터 전달할 api 클래스 작성

---
+++ test/data_source/mask_api_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mask_inventory_app/data/data_source/mask_api.dart';
+import 'package:mask_inventory_app/data/dto/mask_dto.dart';
+
+void main() {
+  test('mask api의 데이터가 dto에 전달되는지 테스트', () async {
+    final MaskApi api = MaskApi();
+    MaskDto dto = await api.getMaskResult();
+    expect(dto, isNotNull);
+  });
+}

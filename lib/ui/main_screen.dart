fc91c8c845a9f28c3ee49857c588f172399ab512
shin-SeungHun <sng7387@gmail.com>
2023-12-31 22:09:44 +0900

    판매수량 UI 수정

--- lib/ui/main_screen.dart
+++ lib/ui/main_screen.dart
@@ -57,33 +57,75 @@ class _MainScreenState extends State<MainScreen> {
         child: isLoading
             ? loadingWidget()
             : ListView.builder(
-          itemCount: storeList.length,
-          itemBuilder: (BuildContext context, int index) {
-            // print(storeList.toString());
-            final Store store = storeList[index];
-            // print('재고 수량 : ${store.remainStat}');
+                itemCount: storeList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  // print(storeList.toString());
+                  final Store store = storeList[index];
+                  // print('재고 수량 : ${store.remainStat}');
 
-            return ListTile(
-              title: Text(store.name),
-              subtitle: Text(store.addr),
-              trailing: Text(store.remainStat),
-            );
-          },
-        ),
+                  return ListTile(
+                    title: Text(store.name),
+                    subtitle: Text(store.addr),
+                    trailing: _buildRemainStatWidget(store: store),
+                  );
+                },
+              ),
       ),
     );
   }
 
   Widget loadingWidget() {
-    return Center(
+    return const Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text('정보를 가져오는 중'),
-          SizedBox(height: 24,),
+          SizedBox(
+            height: 24,
+          ),
           CircularProgressIndicator(),
         ],
       ),
+    );
+  }
+
+  Widget _buildRemainStatWidget({required Store store}) {
+    String remainStat = '판매중지';
+    String description = '판매중지';
+    Color color = Colors.black;
+    switch (store.remainStat) {
+      case 'plenty':
+        remainStat = '충분';
+        description = '100개 이상';
+        color = Colors.green;
+        break;
+      case 'some':
+        remainStat = '보통';
+        description = '30 ~ 100개';
+        color = Colors.yellow;
+        break;
+      case 'few':
+        remainStat = '부족';
+        description = '2 ~ 30개';
+        color = Colors.red;
+        break;
+      case 'empty':
+        remainStat = '소진 임박';
+        description = '1개 이상';
+        color = Colors.grey;
+        break;
+      default:
+    }
+    return Column(
+      children: [
+        Text(
+          remainStat,
+          style: TextStyle(color: color, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          description,
+        ),
+      ],
     );
   }
 }
